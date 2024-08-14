9beac0ee50538808f37bfbcd62092fbb2698dbf0
BuildTools <unconfigured@null.spigotmc.org>
2024-08-14 15:13:03 +1000

    Added messages list

--- index.js
+++ index.js
@@ -1,47 +1,63 @@
-const idSpan = document.getElementById('idSpan');
-const connectButton = document.getElementById('connectButton');
-const connectInput = document.getElementById('connectInput');
-const sendButton = document.getElementById('sendButton');
-const sendInput = document.getElementById('sendInput');
-
-let conn = null;
-let peer = new Peer();
-
-peer.on('open', (id) => {
-    idSpan.innerText = id;
-});
-
-peer.on('connection', (c) => {
-    conn = c;
-    conn.on('open', function() {
-        conn.on('data', function(data) {
-          console.log('Received', data);
-        });
-    });
-});
-
 document.addEventListener('DOMContentLoaded', () => {
+    const idSpan = document.getElementById('idSpan');
+    const connectButton = document.getElementById('connectButton');
+    const connectInput = document.getElementById('connectInput');
+    const sendButton = document.getElementById('sendButton');
+    const sendInput = document.getElementById('sendInput');
+    const messages = document.getElementById('messages');
+    
+    let conn = null;
+    let peer = new Peer();
+    
+    peer.on('open', (id) => {
+        idSpan.innerText = id;
+    });
+    
+    peer.on('connection', (c) => {
+        setupConnection(c);
+    });
+
+    peer.on('disconnected', () => {
+        sendButton.disabled = true;
+        sendInput.disabled = true;
+        log("[sys]: Disconnected from peer");
+    })
 
     connectButton.addEventListener('click', connectToSession);
     sendButton.addEventListener('click', sendMessage);
 
     function connectToSession() {
-        conn = peer.connect(connectInput.value);
+        if(peer.id) {
+            setupConnection(peer.connect(connectInput.value));
+        }
+    }
+
+    function log(s) {
+        element = document.createElement('span');
+        element.innerText = s + '\n';
+        messages.appendChild(element);
+    }
+
+    function sendMessage() {
+        if(conn.open) {
+            msg = sendInput.value;
+            conn.send(msg);
+            log(`[You]: ${msg}`);
+
+        }
+    }
+
+    function setupConnection(c) {
+        conn = c;
         conn.on('open', function() {
+            sendButton.disabled = false;
+            sendInput.disabled = false;
+            log(`[sys] Connected to peer with ID: ${conn.peer}`);
             conn.on('data', function(data) {
-              console.log('Received', data);
+                log(`[Peer]: ${data}`);
             });
         });
     }
 
-    function sendMessage() {
-        if(conn) {
-            conn.send(sendInput.value)
-        }
-    }
-
 });
 
-function setupConnection(c) {
-
-}
